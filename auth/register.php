5690aff2aa910c866dca384089efca0128497391
Jankku <29043938+Jankku@users.noreply.github.com>
2021-11-24 14:42:08 +0200

    Add register route

---
+++ auth/register.php
@@ -0,0 +1,28 @@
+<?php
+
+require "../include/headers.php";
+require "../include/functions.php";
+
+/**
+ * Register a new user
+ */
+try {
+    $db = getConnection();
+    $body = json_decode(file_get_contents('php://input'));
+
+    $username = filter_var($body->username, FILTER_SANITIZE_STRING);
+    $password = password_hash(filter_var($body->password, FILTER_SANITIZE_STRING), PASSWORD_DEFAULT);
+    $firstName = filter_var($body->firstname, FILTER_SANITIZE_STRING);
+    $lastName = filter_var($body->lastname, FILTER_SANITIZE_STRING);
+    $email = filter_var($body->email, FILTER_SANITIZE_STRING);
+    $address = filter_var($body->address, FILTER_SANITIZE_STRING);
+    $city = filter_var($body->city, FILTER_SANITIZE_STRING);
+    $postalCode = filter_var($body->postal_code, FILTER_SANITIZE_STRING);
+
+    $params = [null, false, $username, $password, $firstName, $lastName, $email, $address, $city, $postalCode];
+    $sql = "INSERT INTO user VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+    responseString($db, $sql, "Käyttäjä rekisteröity", "Virhe. Käyttäjää ei rekisteröity", $params);
+} catch (Exception $e) {
+    responseError($e);
+}
