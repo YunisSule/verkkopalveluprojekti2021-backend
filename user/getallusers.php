ef73403af028ad155d2542b730eb2171168db4ba
Jankku <29043938+Jankku@users.noreply.github.com>
2021-11-24 11:19:48 +0200

    Remove password from getalluserdata response and rename file

--- user/getallusers.php
+++ user/getallusers.php
@@ -5,11 +5,11 @@ require "../include/functions.php";
 
 /**
  * Get all userdata
- * Example: /user/getalluserdata.php
+ * Example: /user/getallusers.php
  */
 try {
     $db = getConnection();
-    responseAsJson($db, "SELECT * FROM user", PDO::FETCH_ASSOC);
+    responseAsJson($db, "SELECT user_id, is_admin, username, firstname, lastname, email, address, city, postal_code FROM user", PDO::FETCH_ASSOC);
 } catch (Exception $e) {
     responseError($e);
 }
