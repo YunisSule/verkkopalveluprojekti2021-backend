325608685682ade05d0b69a5e7b1a0dd6a5e794c
Heppikevonen <hegezu@windowslive.com>
2021-12-01 19:14:13 +0200

    login.php muokkaus

--- auth/login.php
+++ auth/login.php
@@ -10,20 +10,52 @@ try {
     session_start();
 
     $db = getConnection();
-    $body = json_decode(file_get_contents('php://input'));
+    //$body = json_decode(file_get_contents('php://input'));
 
-    $username = filter_var($body->username, FILTER_SANITIZE_STRING);
-    $password = filter_var($body->password, FILTER_SANITIZE_STRING);
+    if( isset($_SERVER['PHP_AUTH_USER']) ){
+        $email = filter_var($_SERVER['PHP_AUTH_USER'], FILTER_SANITIZE_EMAIL);
+        $password = filter_var($_SERVER["PHP_AUTH_PW"], FILTER_SANITIZE_STRING);
+        $userdetails = getQueryResult($db, "SELECT username, password FROM user WHERE email = ?", PDO::FETCH_ASSOC, [$email]);
 
-    $hashedPassword = getQueryResult($db, "SELECT password FROM user WHERE username = ?", PDO::FETCH_COLUMN, [$username]);
-    if ($username && password_verify($password, $hashedPassword[0])) {
-        $_SESSION['username'] = $username;
-        $response = ["success" => true];
-    } else {
-        $response = ["success" => false];
+        foreach($userdetails as $row) {
+            $hashedPassword = $row["password"];
+            $username = $row['username'];
+        }
+           
+        if ($email && password_verify($password, $hashedPassword)) {
+            $_SESSION['username'] = $email;
+            $response = ["success" => true];
+        } else {
+            $response = ["success" => false];
+            header('HTTP/1.1 401');
+            exit;
+        }
     }
 
     echo json_encode($response);
 } catch (Exception $e) {
     responseError($e);
 }
+
+
+ // try {
+//     session_start();
+
+//     $db = getConnection();
+//     $body = json_decode(file_get_contents('php://input'));
+
+//     $username = filter_var($body->username, FILTER_SANITIZE_STRING);
+//     $password = filter_var($body->password, FILTER_SANITIZE_STRING);
+
+//     $hashedPassword = getQueryResult($db, "SELECT password FROM user WHERE username = ?", PDO::FETCH_COLUMN, [$username]);
+//     if ($username && password_verify($password, $hashedPassword[0])) {
+//         $_SESSION['username'] = $username;
+//         $response = ["success" => true];
+//     } else {
+//         $response = ["success" => false];
+//     }
+
+//     echo json_encode($response);
+// } catch (Exception $e) {
+//     responseError($e);
+// }
