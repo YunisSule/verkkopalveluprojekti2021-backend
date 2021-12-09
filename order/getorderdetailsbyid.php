6b0b368072a737a9dee9c77192653c2b65e7f295
Jankku <29043938+Jankku@users.noreply.github.com>
2021-12-09 18:02:55 +0200

    Add getorderdetailsbyid.php

---
+++ order/getorderdetailsbyid.php
@@ -0,0 +1,30 @@
+<?php
+
+require "../include/headers.php";
+require "../include/functions.php";
+
+/**
+ * Get all orders of a user
+ * Example: /order/getorderdetailsbyid.php?id=1
+ */
+
+try {
+    $db = getConnection();
+    $orderId = filter_var($_GET['id'], FILTER_SANITIZE_NUMBER_INT);
+
+    if (!is_numeric($orderId)) {
+        throw new Exception("ID:n täytyy olla numero!");
+    }
+
+    $sql = "SELECT orow.order_id, orow.order_row, p.product_id, p.name, orow.quantity, p.price, (orow.quantity * p.price) AS total, o.order_date
+            FROM `order` o
+            INNER JOIN order_row orow ON o.order_id = orow.order_id
+            INNER JOIN product p ON p.product_id = orow.product_id
+            INNER JOIN user u ON u.user_id=o.user_id
+            WHERE o.order_id=?
+            ORDER BY o.order_id, orow.order_row";
+
+    responseAsJson($db, $sql, PDO::FETCH_ASSOC, [$orderId]);
+} catch (Exception $e) {
+    responseError($e);
+}
