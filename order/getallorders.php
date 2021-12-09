d08677788562821b401891ca3d93d15cf82dcc12
Jankku <29043938+Jankku@users.noreply.github.com>
2021-12-09 18:03:39 +0200

    Change existing order routes

--- order/getallorders.php
+++ order/getallorders.php
@@ -4,18 +4,13 @@ require "../include/headers.php";
 require "../include/functions.php";
 
 /**
- * Get all orders
+ * Get all orders for admin panel
  * Example: /order/getallorders.php
  */
 
 try {
     $db = getConnection();
-
-    $sql = "SELECT o.order_id, orow.order_row, orow.product_id, o.user_id, o.state, o.payment_method, orow.quantity, (orow.quantity * p.price) AS price, o.order_date
-            FROM `order` o
-            INNER JOIN order_row orow ON o.order_id = orow.order_id
-            INNER JOIN product p ON p.product_id = orow.product_id
-            ORDER BY o.order_id, orow.order_row";
+    $sql = "SELECT order_id, user_id, state, payment_method, order_date FROM `order`";
 
     responseAsJson($db, $sql, PDO::FETCH_ASSOC);
 } catch (Exception $e) {
--- order/getordersbyuserid.php
+++ order/getordersbyuserid.php
@@ -5,7 +5,7 @@ require "../include/functions.php";
 
 /**
  * Get all orders of a user
- * Example: /order/getallorders.php?user_id=1
+ * Example: /order/getordersbyuserid.php?user_id=1
  */
 
 try {
@@ -16,13 +16,7 @@ try {
         throw new Exception("ID:n täytyy olla numero!");
     }
 
-    $sql = "SELECT o.order_id, orow.order_row, orow.product_id, o.user_id, o.state, o.payment_method, orow.quantity, (orow.quantity * p.price) AS price, o.order_date
-            FROM `order` o
-            INNER JOIN order_row orow ON o.order_id = orow.order_id
-            INNER JOIN product p ON p.product_id = orow.product_id
-            INNER JOIN user u ON u.user_id=o.user_id
-            WHERE o.user_id=?
-            ORDER BY o.order_id, orow.order_row";
+    $sql = "SELECT order_id, state, payment_method, order_date FROM `order` WHERE user_id=?";
 
     responseAsJson($db, $sql, PDO::FETCH_ASSOC, [$userId]);
 } catch (Exception $e) {
